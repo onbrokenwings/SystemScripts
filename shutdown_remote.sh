04a726c4cf6aeeee12ca2d328bf0f3a19f1dd94e
onbrokenwings <israel_llata@yahoo.es>
2016-02-09 13:54:11 +0100

    Format details and comments for script

--- shutdown_remote.sh
+++ shutdown_remote.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
-# Script to shutdown remote machine
+# Script to shutdown remote machine through Airytec Switch Off
 
 host='192.168.1.100'
 site='http://'$host':8000/?action=System.Shutdown'
@@ -9,7 +9,7 @@ ERROR=$(wget -q --spider $site 2>&1)
 
 if [ "$ERROR" = "" ]
 then
-	logger -s 'Shutdown to remote host was executed:' ${host}
+	logger -s 'Shutdown to remote host was executed:' $host
 else
-        logger -s 'Shutdown request was incorrect:' $ERROR
+	logger -s 'Shutdown request was incorrect:' $ERROR
 fi
